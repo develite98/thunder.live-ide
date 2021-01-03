a9f91827e9e0a094ed38848f2cf8e3893065b786
Phong Cao <phongcao3091998@gmail.com>
2021-01-03 20:58:23 +0700

    docs(eslint): add eslint rule: prefer-literal-enum-member
    BREAKING CHANGE:
    Apply prefer enum initializing
    BREAKING CHANGE:
    Apply Eslint Rule: Prefer Enum
    Initializers

--- libs/eslint/src/typescript/best-practices/prefer-literal-enum-member.ts
+++ libs/eslint/src/typescript/best-practices/prefer-literal-enum-member.ts
@@ -1,6 +1,33 @@
+// TODO: Review later
+/* eslint-disable @typescript-eslint/quotes */
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 // COMMENT THE DISABLED ESLINT RULES BELOW TO SEE MORE ERRORS
 /* eslint-disable @typescript-eslint/prefer-literal-enum-member */
 
 // "@typescript/eslint/prefer-literal-enum-member": ["error"],
 // Require that all enum members be literal values to prevent unintended enum member name shadow issues
 // https://github.com/typescript-eslint/typescript-eslint/blob/v4.10.0/packages/eslint-plugin/docs/rules/prefer-literal-enum-member.md
+
+
+// Bad
+const outScopeVariable: number = 1;
+enum BadValueAssignment {
+  A = outScopeVariable, // <= Variable assignment
+  B = {}, // <= Object assignment
+  D = `A ${value} template literal string`, // <= Template literal
+  E = new Set(outScopeVariable, outScopeVariable), // <= Constructor in assignment
+  F = outScopeVariable + outScopeVariable // <= Expression assignment
+}
+
+// Good
+enum GoodValueAssignment {
+  A = false,
+  B = 'TestStr', // A regular string
+  C = 4, // A number
+  D = null,
+  E = /some_regex/
+}
+
+
